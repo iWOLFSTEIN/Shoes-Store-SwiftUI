394db6066a3ba028573908afe61cee9a07497979
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-06-12 14:07:19 +0500

    Initial Commit

---
+++ Shoes Store/Shoes_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoes_StoreApp.swift
+//  Shoes Store
+//
+//  Created by BrainX Technologies on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shoes_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
