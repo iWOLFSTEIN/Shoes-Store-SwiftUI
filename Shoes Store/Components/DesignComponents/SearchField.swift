e0ec95d600872de1c5c4ebea8dfed09e8d8543fa
Talha <talha@EAPPLEs-MacBook-Pro.local>
2023-06-13 19:04:15 +0500

    cart items badge count created

--- Shoes Store/Components/DesignComponents/SearchField.swift
+++ Shoes Store/Components/DesignComponents/SearchField.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct SearchField: View {
     
     let textFieldBackGroundColor = Color("textFieldColor")
+    var cartItemsCount = 12
     
     var body: some View {
         HStack (spacing: 16){
@@ -19,11 +20,24 @@ struct SearchField: View {
                 .background(textFieldBackGroundColor)
                 .cornerRadius(10)
             
-            Image("cart")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 30, height: 30)
-                .padding(.trailing)
+            ZStack (alignment: .topTrailing) {
+                Image("cart")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .padding(.trailing)
+               
+                if cartItemsCount != 0 {
+                    Text("\(cartItemsCount)")
+                        .font(.system(size: 6))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 3, leading: 3, bottom: 3, trailing: 3))
+                        .background(.red)
+                        .cornerRadius(10)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12))
+                }
+            }
         }
         .padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
     }
